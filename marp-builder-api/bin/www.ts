c4a7db9c6f310af5c49fe0ff4a06f83614e4c591
Takumi Ikeda <tkik13201@gmail.com>
2024-05-13 03:56:36 +0900

    fix: change port to 3000

--- marp-builder-api/bin/www.ts
+++ marp-builder-api/bin/www.ts
@@ -14,7 +14,7 @@ const debugLogger = debug('marp-builder-api:server');
  * Get port from environment and store in Express.
  */
 
-const port = normalizePort(process.env.PORT || '80');
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 /**
